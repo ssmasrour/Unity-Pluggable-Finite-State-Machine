8ed55a80fc7dce4c70d47a28f2b577b1b12974b1
Sahab Masrour <sahab.masroor@gmail.com>
2024-05-15 20:30:31 +0330

    NPC eye sight range is about 90 degree. and can't see behind obstacle

--- Assets/Modules/AI/NPC/AI/Decision/DecisionTargetInRange.cs
+++ Assets/Modules/AI/NPC/AI/Decision/DecisionTargetInRange.cs
@@ -8,15 +8,42 @@ namespace Sahab.AI
     public class DecisionTargetInRange : IDecision
     {
         public float checkingRange;
+        public float viewAngle = 45.0f;
+        public LayerMask obstacleMask;
 
         public override bool Check(StateController fsm)
         {
-            if (Vector3.Distance(fsm.transform.position, fsm.Player.position) < checkingRange)
+            if (CanSeePlayer(fsm.transform, fsm.Player))
             {
                 return true;
             }
 
             return false;
         }
+
+    bool CanSeePlayer(Transform me,Transform player)
+    {
+        Vector3 direction = (player.position - me.position).normalized;
+        float distance = Vector3.Distance(me.position, player.position);
+
+        if (distance <= checkingRange)
+        {
+
+            if (Vector3.Angle(me.forward, direction) < viewAngle / 2)
+            {
+
+                RaycastHit hit;
+                if (Physics.Raycast(me.position, direction, out hit, distance, obstacleMask))
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        // Player not within view radius or angle, or obstructed
+        return false;
     }
+    }
+
 }
